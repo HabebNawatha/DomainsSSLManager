c80aef7d915e549961f358e49876d2d55ebd35b9
Habeb Nawatha <109216430+HabebNawatha@users.noreply.github.com>
2024-03-03 13:02:18 +0200

    Edit HomePage using CustomisedModal

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -1,19 +1,24 @@
-import { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import '../assets/styles/HomePageStyle.css';
-import CustomizedButton from '../components/ui/CustomizedButton';
+import CustomisedButton from '../components/ui/CustomisedButton';
 import { CertificationData } from '../models/CertificationData';
+import useFetchData from '../hooks/useFetchData';
+import axios, { AxiosResponse } from 'axios';
+import CustomisedModal from '../components/ui/CustomisedModal';
+import renderCertificateData from '../utils/renderCertificateData';
 
 export default function HomePage() {
     const [url, setUrl] = useState('');
     const [certificateData, setCertificateData] = useState<CertificationData | null>(null);
     const [customrError, setError] = useState<string | null>(null);
+    const [showModal, setShowModal] = useState(false);
 
 
     const fetchData = async () => {
         try {
             if (!url) return; // Skip data fetch if URL is empty
 
-            const response = await fetch(`http://localhost:8000/ssl-service/certificate?domain=${url}`, {
+            const response = await fetch(`http://localhost:8000/certificate/certificate?domain=${url}`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
@@ -27,6 +32,7 @@ export default function HomePage() {
                     responseData.data
                 );
                 setCertificateData(certificate);
+                setShowModal(true);
                 setError(null); // Clear error if successful
             } else {
                 setError(`Failed to fetch data: ${response.statusText}`);
@@ -37,14 +43,24 @@ export default function HomePage() {
     };
 
     const handleSubmit = async () => {
-        fetchData();
+        setShowModal(true)
+        //fetchData();
     };
-
+    const mockCertificateData = {
+        domain: 'google.com',
+        certificateDetails: {
+            issuer: { C: 'US', O: 'Google Trust Services LLC', CN: 'GTS CA 1C3' },
+            validFrom: 'Feb  5 08:03:56 2024 GMT',
+            validTo: 'Apr 29 08:03:55 2024 GMT',
+            serialNumber: 'C389D85E94B24B6909EC2AA96C658A5A',
+            fingerprint: '15:64:B9:36:57:83:FF:80:1D:11:90:74:39:EC:B1:FC:98:E7:B7:C7'
+        }
+    };
 
     return (
         <div className="home-page-container">
             <div className="home-page-introduction-container">
-                <h1 className="home-page-inroduction-h1">Simplify SSL Certificate Management </h1>
+                <h1 className="home-page-inroduction-h1">Simplify SSL Certificate Management</h1>
                 <h1>Easy Links, Simple as It Gets!</h1>
                 <div className="home-page-link-container">
                     <input
@@ -53,24 +69,20 @@ export default function HomePage() {
                         value={url}
                         onChange={(e) => setUrl(e.target.value)}
                     />
-                    <CustomizedButton buttonText='Submit' onClick={handleSubmit} />
-                </div>
+                    <CustomisedButton onClick={handleSubmit}>Submit
+                    </CustomisedButton>                </div>
             </div>
-            <div className="certificate-data-container">
-
-                {certificateData && (
-                    <div>
-                        <h2>Certificate Data</h2>
-                        <p>Domain: {certificateData.domain}</p>
-                        <p>Issuer: {certificateData.certificateDetails.issuer.CN}</p>
-                        <p>Valid From: {certificateData.certificateDetails.validFrom}</p>
-                        <p>Valid To: {certificateData.certificateDetails.validTo}</p>
-                        <p>Serial Number: {certificateData.certificateDetails.serialNumber}</p>
-                        <p>Fingerprint: {certificateData.certificateDetails.fingerprint}</p>
-                    </div>
+            
+                {mockCertificateData && showModal && (
+                    
+                    <CustomisedModal title="Certificate Data" onClose={() => setShowModal(false)}> {/* Render the Modal component */}
+                    {renderCertificateData(mockCertificateData)}
+                    </CustomisedModal>
                 )}
                 {customrError && <p className="error-message">{customrError}</p>}
-            </div>
+            
         </div>
     );
 }
+
+
