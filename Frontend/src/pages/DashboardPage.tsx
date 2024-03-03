337a5ee759abb971cbbb089235f8e0ffc05df2c9
Habeb Nawatha <109216430+HabebNawatha@users.noreply.github.com>
2024-03-03 12:55:47 +0200

    Edit DashboardPage.tsx sorting functionality

--- Frontend/src/pages/DashboardPage.tsx
+++ Frontend/src/pages/DashboardPage.tsx
@@ -1,10 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardContent, CardHeader, Typography, Grid, Container } from '@mui/material';
-import DomainCard from '../components/ui/DomainCard' // Import your DomainCard component
-import '../assets/styles/DashboardPageStyle.css'
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    Typography,
+    Grid,
+    Container,
+    Select,
+    MenuItem,
+    IconButton,
+    InputLabel,
+    FormControl,
+    FormHelperText,
+} from '@mui/material';
+import DomainCard from '../components/ui/DomainCard'; // Import your DomainCard component
+import '../assets/styles/DashboardPageStyle.css';
+import { CertificationData } from '../models/CertificationData';
+import { sortByDomainNameAsc, sortByDomainNameDesc, sortByValidToDateAsc, sortByValidToDateDesc } from '../models/CertificationData';
+
 
 // Replace with your actual data or data fetching logic
-const domainData = [
+const domainData: CertificationData[] = [
     {
         domain: 'google.com',
         certificateDetails: {
@@ -16,26 +32,26 @@ const domainData = [
         },
     },
     {
-        domain: 'google.com',
+        domain: 'youtube.com',
         certificateDetails: {
             issuer: { C: 'US', O: 'Google Trust Services LLC', CN: 'GTS CA 1C3' },
             validFrom: 'Jan 29 08:04:47 2024 GMT',
-            validTo: 'Apr 22 08:04:46 2024 GMT',
+            validTo: 'Apr 21 08:04:46 2024 GMT',
             serialNumber: '929CDF7F59C08AE9099B8CC9716EB04A',
             fingerprint: '66:92:08:3D:8D:29:C3:CF:50:3F:34:A3:87:1B:18:29:A9:9A:66:A2',
         },
     },
     {
-        domain: 'google.com',
+        domain: 'Linkedin.com',
         certificateDetails: {
             issuer: { C: 'US', O: 'Google Trust Services LLC', CN: 'GTS CA 1C3' },
             validFrom: 'Jan 29 08:04:47 2024 GMT',
-            validTo: 'Apr 22 08:04:46 2024 GMT',
+            validTo: 'Apr 23 08:04:46 2024 GMT',
             serialNumber: '929CDF7F59C08AE9099B8CC9716EB04A',
             fingerprint: '66:92:08:3D:8D:29:C3:CF:50:3F:34:A3:87:1B:18:29:A9:9A:66:A2',
         },
     },
-    
+
 ];
 
 
@@ -43,14 +59,49 @@ const domainData = [
 const DashboardPage = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [selectedFilter, setSelectedFilter] = useState<string | null>(null);
+    const applyFilter = (data: CertificationData[], filter: string): CertificationData[] => {
+        switch (filter) {
+            case 'domainNameAsc':
+                return sortByDomainNameAsc(data);
+            case 'domainNameDesc':
+                return sortByDomainNameDesc(data);
+            case 'expiryDateAsc':
+                return sortByValidToDateAsc(data);
+            case 'expiryDateDesc':
+                return sortByValidToDateDesc(data);
+            default:
+                return data;
+        }
+    };
+
+    const filteredData = selectedFilter ? applyFilter(domainData, selectedFilter) : domainData; // Sorted data based on filter (if available)
+
+    const handleFilterChange = (event: any) => {
+        setSelectedFilter(event.target.value as string | null);
+    };
+
+
 
     return (
-        <Container maxWidth="xl" >
+        <Container maxWidth="lg">
             <Grid container spacing={3}>
-                <Grid item xs={12} mb={4}>
-                    <Typography variant="h3" align="center" className='grid-header'>
-                        Domain Certificates Dashboard
-                    </Typography>
+                <Grid container spacing={2} alignItems="center" mb={2} item xs={12}>
+                    <FormControl sx={{ m: 1, minWidth: 120 }}>
+                        <InputLabel id="demo-simple-select-helper-label">Sort By</InputLabel>
+                        <Select
+                            value={selectedFilter}
+
+                            label="Sort By"
+                            onChange={handleFilterChange}
+                            fullWidth
+                        >
+                            <MenuItem value="domainNameAsc">Domain Name (A-Z)</MenuItem>
+                            <MenuItem value="domainNameDesc">Domain Name (Z-A)</MenuItem>
+                            <MenuItem value="expiryDateAsc">Expiry Date (Ascending)</MenuItem>
+                            <MenuItem value="expiryDateDesc">Expiry Date (Descending)</MenuItem>
+                        </Select>
+                    </FormControl>
                 </Grid>
                 {loading ? (
                     <Grid item xs={12} alignItems="center">
@@ -62,7 +113,7 @@ const DashboardPage = () => {
                     </Grid>
                 ) : (
                     <Grid container spacing={4}>
-                        {domainData.map((domain) => (
+                        {filteredData.map((domain: CertificationData) => (
                             <Grid item key={domain.domain} xs={12} sm={6} md={4}>
                                 <DomainCard
                                     key={domain.domain}
@@ -77,6 +128,5 @@ const DashboardPage = () => {
         </Container>
     );
 };
-
 
 export default DashboardPage;
