e148deef0af770657962ebb553607aa55e864c1d
Habeb Nawatha <109216430+HabebNawatha@users.noreply.github.com>
2024-07-21 17:17:14 +0300

    Modifying dashboard page

--- Frontend/src/pages/DashboardPage.tsx
+++ Frontend/src/pages/DashboardPage.tsx
@@ -1,32 +1,56 @@
 import React, { useState, useEffect } from 'react';
-import {
-    Card,
-    CardContent,
-    CardHeader,
-    Typography,
-    Grid,
-    Container,
-    Select,
-    MenuItem,
-    IconButton,
-    InputLabel,
-    FormControl,
-    FormHelperText,
-} from '@mui/material';
-import DomainCard from '../components/ui/DomainCard'; // Import your DomainCard component
-import '../assets/styles/DashboardPageStyle.css';
-import { CertificationData } from '../models/CertificationData';
-import { sortByDomainNameAsc, sortByDomainNameDesc, sortByValidToDateAsc, sortByValidToDateDesc } from '../models/CertificationData';
 import axios from 'axios';
+import CertificatesToolbar from '../components/ui/DashboardToolbar';
+import DomainCard from '../components/ui/DomainCard';
+import {
+    sortByDomainNameAsc,
+    sortByDomainNameDesc,
+    sortByValidToDateAsc,
+    sortByValidToDateDesc,
+    CertificationData,
+} from '../models/CertificationData';
+import '../assets/styles/DashboardPageStyle.css';
+import { Delete } from '@mui/icons-material';
 
-const DashboardPage = () => {
+const DashboardPage: React.FC = () => {
+    const initialDomainData: CertificationData[] = [
+        {
+            domain: 'google.com',
+            certificateDetails: {
+                issuer: { C: 'US', O: 'Google Trust Services LLC', CN: 'GTS CA 1C3' },
+                validFrom: 'Jan 29 08:04:47 2024 GMT',
+                validTo: 'Apr 22 08:04:46 2024 GMT',
+                serialNumber: '929CDF7F59C08AE9099B8CC9716EB04A',
+                fingerprint: '66:92:08:3D:8D:29:C3:CF:50:3F:34:A3:87:1B:18:29:A9:9A:66:A2',
+            },
+        },
+        {
+            domain: 'youtube.com',
+            certificateDetails: {
+                issuer: { C: 'US', O: 'Google Trust Services LLC', CN: 'GTS CA 1C3' },
+                validFrom: 'Jan 29 08:04:47 2024 GMT',
+                validTo: 'Apr 21 08:04:46 2024 GMT',
+                serialNumber: '929CDF7F59C08AE9099B8CC9716EB04A',
+                fingerprint: '66:92:08:3D:8D:29:C3:CF:50:3F:34:A3:87:1B:18:29:A9:9A:66:A2',
+            },
+        },
+        {
+            domain: 'linkedin.com',
+            certificateDetails: {
+                issuer: { C: 'US', O: 'Google Trust Services LLC', CN: 'GTS CA 1C3' },
+                validFrom: 'Jan 29 08:04:47 2024 GMT',
+                validTo: 'Apr 23 08:04:46 2024 GMT',
+                serialNumber: '929CDF7F59C08AE9099B8CC9716EB04A',
+                fingerprint: '66:92:08:3D:8D:29:C3:CF:50:3F:34:A3:87:1B:18:29:A9:9A:66:A2',
+            },
+        },
+    ];
 
+    const [certificates, setCertificates] = useState<CertificationData[]>(initialDomainData);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
-    const [selectedFilter, setSelectedFilter] = useState<string | null>(null);
-    const [certificates, setCertificates] = useState<CertificationData[]>([]);
+    const [isEditMode, setIsEditMode] = useState(false);
 
-    //Fetch certificates for user.
     useEffect(() => {
         const fetchCertificates = async () => {
             setLoading(true);
@@ -47,69 +71,60 @@ const DashboardPage = () => {
         fetchCertificates();
     }, []);
 
-    //Applying filter
-    const applyFilter = (data: CertificationData[], filter: string): CertificationData[] => {
-        switch (filter) {
-            case 'domainNameAsc':
-                return sortByDomainNameAsc(data);
-            case 'domainNameDesc':
-                return sortByDomainNameDesc(data);
-            case 'expiryDateAsc':
-                return sortByValidToDateAsc(data);
-            case 'expiryDateDesc':
-                return sortByValidToDateDesc(data);
+    const handleSortChange = (sortOption: string) => {
+        let sortedCertificates: CertificationData[];
+        switch (sortOption) {
+            case 'domainAsc':
+                sortedCertificates = sortByDomainNameAsc(certificates);
+                break;
+            case 'domainDesc':
+                sortedCertificates = sortByDomainNameDesc(certificates);
+                break;
+            case 'validToAsc':
+                sortedCertificates = sortByValidToDateAsc(certificates);
+                break;
+            case 'validToDesc':
+                sortedCertificates = sortByValidToDateDesc(certificates);
+                break;
             default:
-                return data;
+                sortedCertificates = certificates;
         }
+        setCertificates([...sortedCertificates]); // Ensure state update triggers re-render
     };
-    const filteredData = selectedFilter ? applyFilter(certificates, selectedFilter) : certificates;
-    const handleFilterChange = (event: any) => {
-        setSelectedFilter(event.target.value as string | null);
-    };
+    const handleEditClick = () => {
+        setIsEditMode(!isEditMode);
+    }
+    const handleDeleteCard = (domain:string) => {
+        setCertificates(certificates.filter(cert => cert.domain !== domain));
+    }
 
     return (
-        <Container maxWidth="lg">
-            <Grid container spacing={3}>
-                <Grid container spacing={2} alignItems="center" mb={2} item xs={12}>
-                    <FormControl sx={{ m: 1, minWidth: 120 }}>
-                        <InputLabel id="demo-simple-select-helper-label">Sort By</InputLabel>
-                        <Select
-                            value={selectedFilter}
-
-                            label="Sort By"
-                            onChange={handleFilterChange}
-                            fullWidth
-                        >
-                            <MenuItem value="domainNameAsc">Domain Name (A-Z)</MenuItem>
-                            <MenuItem value="domainNameDesc">Domain Name (Z-A)</MenuItem>
-                            <MenuItem value="expiryDateAsc">Expiry Date (Ascending)</MenuItem>
-                            <MenuItem value="expiryDateDesc">Expiry Date (Descending)</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Grid>
+        <div>
+            <CertificatesToolbar onSortChange={handleSortChange} onEditClick={handleEditClick} />
+            <div className='certificates-container'>
                 {loading ? (
-                    <Grid item xs={12} alignItems="center">
-                        <Typography variant="body1">Loading data...</Typography>
-                    </Grid>
+                    <div className='loading-certificates'> Loading...</div>
                 ) : error ? (
-                    <Grid item xs={12} alignItems="center">
-                        <Typography variant="body1">Error: {error}</Typography>
-                    </Grid>
+                    <div className='error-certificates'> Error</div>
                 ) : (
-                    <Grid container spacing={4}>
-                        {filteredData.map((domain: CertificationData) => (
-                            <Grid item key={domain.domain} xs={12} sm={6} md={4}>
+                    <div className='projects-section'>
+                        {certificates.map((domain) => (
+                            <div className='domain-card-div'>
                                 <DomainCard
                                     key={domain.domain}
                                     domain={domain.domain}
                                     certificateDetails={domain.certificateDetails}
                                 />
-                            </Grid>
+                                <button className={`card-delete-btn ${isEditMode ? 'visible' : ''}`}
+                                    onClick={() => handleDeleteCard(domain.domain)}>
+                                    <Delete />
+                                </button>
+                            </div>
                         ))}
-                    </Grid>
+                    </div>
                 )}
-            </Grid>
-        </Container>
+            </div>
+        </div>
     );
 };
 
