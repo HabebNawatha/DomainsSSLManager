c5f0ca7677be1c2ead9295e086545f7a0c74de0b
Habeb Nawatha <109216430+HabebNawatha@users.noreply.github.com>
2024-07-22 19:55:59 +0300

    Simple line separator created

---
+++ Frontend/src/components/ui/LineSeparator.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './LineSeparatorStyle.css'; // Make sure to create this CSS file
+
+const LineSeparator = () => {
+  return <div className="line-separator"></div>;
+};
+
+export default LineSeparator;
